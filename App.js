e7a93c89cc209644c280315daaf132feb1666e24
Dennis Lee <dennis@dennislee.xyz>
2023-04-22 16:16:03 -0700

    feat: expose TextInput values as variables

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [l, onChangeL] = useState();
+  const [w, onChangeW] = useState();
+  const [h, onChangeH] = useState();
   return (
     <View style={styles.container}>
       <TextInput
         inputMode="numeric"
+        onChangeText={onChangeL}
+        value={l}
       />
       <TextInput
         inputMode="numeric"
+        onChangeText={onChangeW}
+        value={w}
       />
       <TextInput
         inputMode="numeric"
+        onChangeText={onChangeL}
+        value={h}
       />
       <StatusBar style="auto" />
     </View>
