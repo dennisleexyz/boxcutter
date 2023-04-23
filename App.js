a9d614c2a5fe9ad941018a345f218d3fcef1d0e5
Dennis Lee <dennis@dennislee.xyz>
2023-04-22 23:14:06 -0700

    feat: add box function (TODO: diagonal flap cuts)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import makerjs from 'makerjs';
 
 export default function App() {
   const [l, onChangeL] = useState();
@@ -36,3 +37,41 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function box(l, w, h) {
+  this.units = makerjs.unitType.Inch;
+
+  var paths = [];
+
+  for (const x of [
+    1,
+    1 + l,
+    1 + l + w,
+    1 + 2*l + w,
+  ])
+    paths.push({
+      type: 'line',
+      origin: [x, 0],
+      end: [x, l + h],
+      layer: 'red',
+    });
+
+  for (const y of [
+    l/2,
+    l/2 + h,
+  ])
+    paths.push({
+      type: 'line',
+      origin: [0, y],
+      end: [1 + 2*(l+w), y],
+      layer: 'red',
+    });
+
+  for (const x of [
+    1 + l/2,
+    1 + 3*l/2 + w,
+  ])
+
+  this.paths = paths;
+  this.models = [new makerjs.models.Rectangle(1 + 2*(l+w), l + h)];
+}
